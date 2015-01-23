e6af813ca8f3bdc1de03e892878d717997a653e1
Isis Anchalee <isiswenger@gmail.com>
2015-01-23 13:34:48 -0800

    working on ai logic

--- js/ttt-ai-player.js
+++ js/ttt-ai-player.js
@@ -10,10 +10,29 @@
     this.poses = [];
   };
 
+  AIPlayer.diagonals = [
+  	[[0, 0], [1, 1], [2, 2]],
+  	[[2, 0], [1, 1], [0, 2]]
+  ]
+
+	AIPlayer.verticals = [
+		[[0, 0], [1, 0], [2, 0]],
+	  [[0, 1], [1, 1], [2, 1]],
+	  [[0, 2], [1, 2], [2, 2]]
+  ]   
+
+  AIPlayer.horizontals = [
+		[[0, 0], [0, 1], [0, 2]],
+	  [[1, 0], [1, 1], [1, 2]],
+	  [[2, 0], [2, 1], [2, 2]]
+  ]
+
   AIPlayer.prototype.playTurn = function(){
   	var nextMove;
+  	var dirs = [AIPlayer.diagonals, AIPlayer.verticals, AIPlayer.horizontals];
+  	console.log("playing!!")
   	for (var i = 0; i < 3; i++){
-  		nextMove = this.checkRows(this, this.board.posSeqs[i])
+  		nextMove = this.checkRows(this, dirs[i]);
   	}
 
   	nextMove ? this.board.placeMark(nextMove, "o") : this.block();
@@ -24,10 +43,10 @@
     for (var i = 0; i < 3; i++){
     	var counter = 0;
     	for (var j = 0; j < 3; j++){
-    		if (player.poses.indexOf(posSeqs[i][j]) !== - 1){
+    		if (player.poses.indexOf(posSeqs[0][i][j]) !== - 1){
     			counter += 1;
-    		} else if (player.poses.indexOf(posSeqs[i][j]) === - 1 && counter == 2){
-    			playPos = posSeqs[i][j];
+    		} else if (player.poses.indexOf(posSeqs[0][i][j]) === - 1 && counter == 2){
+    			playPos = posSeqs[0][i][j];
     		}
     	}
     }
@@ -36,12 +55,12 @@
   };
 
  AIPlayer.prototype.block = function(){
-  	var nextMove;
-  	for (var i = 0; i < 3; i++){
-  		nextMove = this.checkRows(this.enemy, this.board.posSeqs[i])
-  	}
+  	// var nextMove;
+  	// for (var i = 0; i < 3; i++){
+  	// 	nextMove = this.checkRows(this.enemy, this.board.posSeqs[0][i])
+  	// }
 
-  	nextMove ? this.board.placeMark(nextMove, "o") : this.fork();
+  	// nextMove ? this.board.placeMark(nextMove, "o") : this.fork();
   };
 
   AIPlayer.prototype.fork = function(){
