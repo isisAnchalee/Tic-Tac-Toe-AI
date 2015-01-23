0c8789d951fff20e2a351df6eb192af4210e0f5b
Isis Anchalee <isiswenger@gmail.com>
2015-01-22 19:31:58 -0800

    add ai player

---
+++ js/ttt-ai-player.js
@@ -0,0 +1,16 @@
+(function () {
+  if (typeof TTT === "undefined") {
+    window.TTT = {};
+  }
+
+  var AIPlayer = TTT.AIPlayer = function () {
+	  this.game = game;
+    this.sign = "O"
+  };
+
+  AIPlayer.prototype.makeMove = function(){
+
+  };
+
+
+})();
---
+++ js/ttt-human-player.js
@@ -0,0 +1,16 @@
+(function () {
+  if (typeof TTT === "undefined") {
+    window.TTT = {};
+  }
+
+  var HumanPlayer = TTT.HumanPlayer = function () {
+	  this.game = game;
+    this.sign = "X"
+  };
+
+  HumanPlayer.prototype.makeMove = function(){
+
+  };
+
+
+})();
