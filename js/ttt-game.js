30812e470c90ff2769a4071159d57cd842731c8d
Isis Anchalee <isiswenger@gmail.com>
2015-01-22 19:29:12 -0800

    add game class

---
+++ js/ttt-game.js
@@ -0,0 +1,24 @@
+(function () {
+  if (typeof TTT === "undefined") {
+    window.TTT = {};
+  }
+
+  var Game = TTT.Game = function (player1, player2) {
+	  this.player1 = player1;
+	  this.player2 = player2;
+  };
+
+  Game.prototype.startGame = function(){
+    while (!this.hasWon()){
+      this.player1.playMove();
+      this.player2.aiMove();
+    }
+
+    endGame();
+  }
+
+  function endGame(){
+    
+  }
+  
+})();
